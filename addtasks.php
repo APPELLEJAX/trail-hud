d3e579bb403d3aa6584cb8b2df034efb786d4aa2
APPELLEJAX <midnbeal18@gmail.com>
2017-04-08 09:24:35 -0400

    pulling nice canvas

---
+++ addtasks.php
@@ -0,0 +1,21 @@
+<?php  ?>
+
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <style type="text/css">
+        #submit{
+          length: 300px;
+          height: 200px;
+          color: #88aa88;
+        }
+      </style>
+    </head>
+    <body>
+
+
+<?php ?>
+
+      <div id="submit">SUBMIT</div>
+    </body>
+  </html>
